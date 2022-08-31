e62fe73ac17632efa842ef53baf24c35709f7d17
Harvey-Specter <2258104212@qq.com>
2022-08-31 11:10:02 +0800

    get new data

---
+++ batchSt.bat
@@ -0,0 +1,5 @@
+python savedate/stooqdata.py 2022-08-23
+python savedate/stooqdata.py 2022-08-24
+python savedate/stooqdata.py 2022-08-25
+python savedate/stooqdata.py 2022-08-26
+python savedate/stooqdata.py 2022-08-29
